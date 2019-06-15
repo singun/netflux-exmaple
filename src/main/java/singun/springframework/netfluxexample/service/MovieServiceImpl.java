342a45a7b0a0a9aa3b4fdad3693049a1ab34ff2e
singun <daewook.shin@gmail.com>
2019-06-16 08:55:40 +0900

    create service layer

--- src/main/java/singun/springframework/netfluxexample/service/MovieServiceImpl.java
+++ src/main/java/singun/springframework/netfluxexample/service/MovieServiceImpl.java
@@ -1,23 +1,38 @@
 package singun.springframework.netfluxexample.service;
 
+import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import singun.springframework.netfluxexample.domain.Movie;
 import singun.springframework.netfluxexample.domain.MovieEvent;
+import singun.springframework.netfluxexample.repositories.MovieRepository;
 
+import java.time.Duration;
+import java.util.Date;
+
+@Service
 public class MovieServiceImpl implements MovieService {
+
+	private final MovieRepository movieRepository;
+
+	public MovieServiceImpl(MovieRepository movieRepository) {
+		this.movieRepository = movieRepository;
+	}
+
 	@Override
 	public Flux<MovieEvent> events(String movieId) {
-		return null;
+		return Flux.<MovieEvent>generate(movieEventSynchronousSink -> {
+			movieEventSynchronousSink.next(new MovieEvent(movieId, new Date()));
+		}).delayElements(Duration.ofSeconds(1));
 	}
 
 	@Override
 	public Mono<Movie> getMovieById(String id) {
-		return null;
+		return this.movieRepository.findById(id);
 	}
 
 	@Override
 	public Flux<Movie> getAllMovies() {
-		return null;
+		return this.movieRepository.findAll();
 	}
 }
