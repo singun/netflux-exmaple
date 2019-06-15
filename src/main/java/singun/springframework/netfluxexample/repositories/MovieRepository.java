fe8836458969fb5fce4a29aeabbb59fd4c1bbe8f
singun <daewook.shin@gmail.com>
2019-06-16 00:18:25 +0900

    create mongo reactive repository

---
+++ src/main/java/singun/springframework/netfluxexample/repositories/MovieRepository.java
@@ -0,0 +1,7 @@
+package singun.springframework.netfluxexample.repositories;
+
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import singun.springframework.netfluxexample.domain.Movie;
+
+public interface MovieRepository extends ReactiveMongoRepository<Movie, String> {
+}
