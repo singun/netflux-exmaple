f12011fe89a9265e610be9b24be601567b0dd30e
singun <daewook.shin@gmail.com>
2019-06-16 00:07:39 +0900

    domain

---
+++ src/main/java/singun/springframework/netfluxexample/domain/Movie.java
@@ -0,0 +1,17 @@
+package singun.springframework.netfluxexample.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+@Data
+@NoArgsConstructor
+public class Movie {
+
+	private String id;
+
+	@NonNull
+	private String title;
+}
