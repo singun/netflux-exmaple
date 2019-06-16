8385f0fd6381ccec59bc5d3ac8967d8f7065f806
singun <daewook.shin@gmail.com>
2019-06-16 09:12:33 +0900

    running the reactive application

--- src/main/java/singun/springframework/netfluxexample/controllers/MovieController.java
+++ src/main/java/singun/springframework/netfluxexample/controllers/MovieController.java
@@ -1,5 +1,6 @@
 package singun.springframework.netfluxexample.controllers;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,7 +22,7 @@ public class MovieController {
 	}
 
 
-	@GetMapping(value = "/{id}/events")
+	@GetMapping(value = "/{id}/events", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
 	Flux<MovieEvent> streamMovieEvents(@PathVariable String id) {
 		return movieService.events(id);
 	}
